c761976c7db3db5e583f878c4a1747069c1a579d
xor <benghia1st@gmail.com>
2022-07-04 19:51:28 +0700

    Update Chap1-Ex01-NumberGuessingGame.py

--- Python Workout 50 Essential Exercises by Reuven M. Lerner/Chap1-Ex01-NumberGuessingGame.py
+++ Python Workout 50 Essential Exercises by Reuven M. Lerner/Chap1-Ex01-NumberGuessingGame.py
@@ -11,10 +11,11 @@ from random import randint as rd
 
 # Function
 def guessing_game():
-    random_number = rd(1, 101) # Random number in range [1, 100]
-    guess = 0
-    while guess != random_number:
-        guess = int(input('Guess it: '))
+    random_number = rd(0, 101) # Random number in range [0, 100]
+    name = input('Enter your name: ')
+    print(f'Hello, {name}!')
+    
+    while guess := int(input('Guess it: ')):
         if guess > random_number:
             print('Too high. Try again!')
             continue
@@ -24,3 +25,6 @@ def guessing_game():
         else:
             print(f'Just right. The number is {guess}')
             return
+
+# Run
+guessing_game()
